fa52a337239fbef393669cc2dcc767f6e4745e1b
krishna9809 <multiatoms980996@gmail.com>
2023-12-14 23:12:41 +0530

    Braid Lunar Website

---
+++ script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  let navMenu = document.getElementById('nav-menu');
+
+const openNav = () => {
+    navMenu.style.display = 'block';
+}
+
+
+const closeNav = () => {
+    navMenu.style.display = 'none';
+}
